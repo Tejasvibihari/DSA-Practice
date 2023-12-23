e6015ec01651ebfa87c543029f7f01f4ff7c4d4f
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2023-12-23 13:33:06 +0545

    Declaration of array

--- Array/index.js
+++ Array/index.js
@@ -1,5 +1,48 @@
+import { Console } from "console";
+import express from "express";
+const app = express();
 
 // Declare of Array 
 
+// First way of declaration
 const cars = ["BMW", "AUDI", "MARUTI"];
 console.log(cars[0]);
+console.log(cars[2]);
+
+// Second way of declaration of array
+
+const cars2 = [];
+cars2[0] = "BMW2";
+console.log(cars2[0]);
+cars2.push("AUDI2");
+console.log(cars2[1]);
+
+// Third way of declaration of array
+
+const cars3 = new Array("BMW3", "AUDI3", "MARUTI3");
+console.log(cars3[2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, (req, res) => {
+    console.log("Server started on port 3000");
+})
