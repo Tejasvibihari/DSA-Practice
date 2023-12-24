098d7d28753637435e5d219294d976cb45fb42de
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2023-12-24 19:58:09 +0545

    Add or Remove element from An Array

--- Array/index.js
+++ Array/index.js
@@ -5,34 +5,44 @@ const app = express();
 // Declare of Array 
 
 // First way of declaration
-const cars = ["BMW", "AUDI", "MARUTI"];
-console.log(cars[0]);
-console.log(cars[2]);
+// const cars = ["BMW", "AUDI", "MARUTI"];
+// console.log(cars[0]);
+// console.log(cars[2]);
 
 // Second way of declaration of array
 
-const cars2 = [];
-cars2[0] = "BMW2";
-console.log(cars2[0]);
-cars2.push("AUDI2");
-console.log(cars2[1]);
+// const cars2 = [];
+// cars2[0] = "BMW2";
+// console.log(cars2[0]);
+// cars2.push("AUDI2");
+// console.log(cars2[1]);
 
 // Third way of declaration of array
 
-const cars3 = new Array("BMW3", "AUDI3", "MARUTI3");
-console.log(cars3[2]);
+// const cars3 = new Array("BMW3", "AUDI3", "MARUTI3");
+// console.log(cars3[2]);
 
 //Array can store any tyoe of data
 
-let person = { name: "Tejasvi Kumar", age: 22 };
+// let person = { name: "Tejasvi Kumar", age: 22 };
 
-const newArr = ["Hello", "Tejasvi", person];
-console.log(newArr[2]);
-console.log(newArr[2].age);
+// const newArr = ["Hello", "Tejasvi", person];
+// console.log(newArr[2]);
+// console.log(newArr[2].age);
 
+// Add or remove element form an array
+const newArr = ["Tejasvi"];
 
+newArr.push("Bihari");
+console.log(newArr);
+newArr.pop();
+console.log(newArr);
 
+newArr.unshift("Hello");
+console.log(newArr);
 
+newArr.shift();
+console.log(newArr);
 
 
 
