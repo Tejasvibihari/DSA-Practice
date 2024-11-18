f9370f8aa179bd313b901a69987a75a91d8bdd68
Tejasvi kumar <38103634+Tejasvibihari@users.noreply.github.com>
2024-11-19 01:47:07 +0530

    Create Basic Recursion Question.cpp

---
+++ Basic Recursion Question.cpp
@@ -0,0 +1,59 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+//   Recursion Basic Question 
+// Print Name 5 Times Start
+// int count = 0;
+// void printName() {
+  
+//     if(count < 5){
+//     cout << "Tejasvi Bihari"<< endl;
+//           count++;
+//  printName();
+//     }
+// }
+// Print Name 5 Times End
+
+
+
+// Print Linearly 1 to N Start
+// int n ;
+// int count = 1;
+
+// void printLinearNumber(){
+//     if(count <= n){
+//         cout << count << endl;
+//         count++;
+//     }
+//     printLinearNumber();
+// }
+// Print Linearly 1 to N End
+
+
+
+// Print Linearly N to 1 Start
+// int n ;
+
+// void printLinearNumber(){
+//     if(n >= 1){
+//         cout << n << endl;
+//         n--;
+//     }
+//     printLinearNumber();
+// }
+// Print Linearly N to 1 End
+
+
+
+
+int main() {
+//   printName();
+
+// Print Linearly 1 to N Start and n to 1
+// cout << "To which Number You have to Print";
+// cin >> n;
+// printLinearNumber();
+// Print Linearly 1 to N End and n to 1
+
+    return 0;
+}
